f68b21a7d85a43d49495e6221269e1ff88d8bcf5
kevin866 <k.wang1502483@gmail.com>
2024-02-23 12:56:19 -0800

    adsf

--- hw5.py
+++ hw5.py
@@ -20,36 +20,35 @@ def temperature_to_volt(desired_temp):
     print("Corresponding PWM value for the desired temperature:", pwm)
     return pwm
 
-# Ask for desired temperature
-T11 = float(input("Enter the T1 temperature in Celius: "))
-T21 = float(input("Enter the T2 temperature in Celius: "))
-T31 = float(input("Enter the T3 temperature in Celius: "))
 
 def update_plot(frame):
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
-        recorded_temp = float(line[0])
-        print("Recorded temperature:", recorded_temp)
+        parts = line.split(',')  # Assuming data is sent as 'temperature,controlEffort'
+        if len(parts) == 4:
+            recorded_temp, T11, T21, T31  = float(parts[0]) , float(parts[1]), float(parts[2]) , float(parts[3])
+            print("Recorded temperature:", recorded_temp, "T1:", T11, "T2:", T21, "T3:", T31)
         
-        # Append data for plotting
-        current_time = time.time()
-        times.append(current_time)
-        # desired_temps.append(pwm_value)
-        recorded_temps.append(recorded_temp)
-        T1.append(T11)
-        T2.append(T21)
-        T3.append(T31)
-        
-        # Update plot
-        plt.cla()
-        plt.plot(times, recorded_temps, label="Recorded Temperature (C)")
-        plt.plot(times, T1, label="T1 Temperature (C)")
-        plt.plot(times, T2, label="T2 Temperature (C)")
-        plt.plot(times, T3, label="T3 Temperature (C)")
-        plt.legend()
-        plt.xlabel('Time')
-        plt.ylabel('Value')
-        plt.tight_layout()
+            
+            # Append data for plotting
+            current_time = time.time()
+            times.append(current_time)
+            # desired_temps.append(pwm_value)
+            recorded_temps.append(recorded_temp)
+            T1.append(T11)
+            T2.append(T21)
+            T3.append(T31)
+            
+            # Update plot
+            plt.cla()
+            plt.plot(times, recorded_temps, label="Recorded Temperature (C)")
+            plt.plot(times, T1, label="T1 Temperature (C)")
+            plt.plot(times, T2, label="T2 Temperature (C)")
+            plt.plot(times, T3, label="T3 Temperature (C)")
+            plt.legend()
+            plt.xlabel('Time')
+            plt.ylabel('Temperature')
+            plt.tight_layout()
     
         # Optionally, send PWM value to Arduino
         # ser.write(str(pwm_value).encode())
