463fa69214ec65e189c2c280b9cdcd0ab0a5afc0
kevin866 <k.wang1502483@gmail.com>
2024-02-25 18:36:36 -0800

    save data

--- graph.py
+++ graph.py
@@ -4,11 +4,11 @@ import time
 import matplotlib.pyplot as plt
 #
 # Load the array from the file
-T1 = np.load('T1.npy')+2.5
-T2 = np.load('T2.npy')
-T3 = np.load('T3.npy')
-recorded_temps = np.load('Temp.npy')
-times = np.load('times.npy')
+T1 = np.load('T11.npy')
+T2 = np.load('T21.npy')
+T3 = np.load('T31.npy')
+recorded_temps = np.load('Temp1.npy')
+times = np.load('times1.npy')
 
 plt.plot(times, recorded_temps, label="Recorded Temperature (C)")
 plt.plot(times, T1, label="T1 Temperature (29.5 C) ")
