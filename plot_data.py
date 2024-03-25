03c79a4e3639d886b953ce0305f526d23f0bd13f
kevin866 <k.wang1502483@gmail.com>
2024-03-24 22:48:08 -0700

    asdf

--- plot_data.py
+++ plot_data.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
+import scipy.io
 # Step 1: Import necessary libraries
 
 # Step 2: Read the .npy data file
 cur_angle = np.load('current angle.npy')
 setpoints = np.load('setpoints.npy')
-scipy.io.savemat('cur_angle.mat', cur_angle)
-scipy.io.savemat('setpoints.mat', setpoints)
+scipy.io.savemat('cur_angle.mat', {'cur_angle': cur_angle})
+scipy.io.savemat('setpoints.mat', {'setpoints': setpoints})
 
 time = np.linspace(0,450,450)/100
 print(time.shape)
-scipy.io.savemat('time.mat', time)
+scipy.io.savemat('time.mat', {'time': time})
 
 # Step 3: Plot the data
 plt.plot(time, cur_angle[0:450], label='Current Angle')
